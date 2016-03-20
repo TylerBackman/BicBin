0261c844175f4e0a8cdd5e90c93870c0afb3a925
Tyler Backman <tbackman@ucr.edu>
2016-03-20 12:48:34 -0700

    reordered code to agree with paper

--- R/run_bicbin.R
+++ R/run_bicbin.R
@@ -1,30 +1,41 @@
 # Written by Tyler William H Backman in 2016
 
-# perform bicbin clustering in parallel
+# Algorithm BicBin
 bicBinCluster <- function(binaryMatrix, totalClusters, alpha=0.5, beta=0.5, minDensityP1=0, tries=900){
-    M <- nrow(binaryMatrix)
-    N <- ncol(binaryMatrix)
-    p <- sum(binaryMatrix) / (M*N)
+    p <- sum(binaryMatrix) / (nrow(binaryMatrix) * ncol(binaryMatrix))
     clusters <- list()
-    for(i in 1:totalClusters){ # this loop implements Algorithm BicBin
-	density <- 0
-	while(density < minDensityP1){ # this loop implements Algorithm A_2
-        	allResults <- foreach(thisTry = 1:tries) %dorng% {
-             	 res1 <- .BicBin(binaryMatrix, alpha, beta, p, proc_genes=TRUE) # Algorithm A_1 by columns
-             	 res2 <- .BicBin(binaryMatrix, alpha, beta, p, proc_genes=FALSE) # Algorithm A_1 by rows
-            	  if(res1$score > res2$score)
-            	    return(res1)
-            	  else
-             	   return(res2)
-        	}
-		max <- allResults[[which.max(sapply(allResults, function(x) x$score))]]
-		subMatrix <- binaryMatrix[max$x == 1, max$y == 1]
-		density <- sum(subMatrix) / (nrow(subMatrix) * ncol(subMatrix))
-        print(paste("density:", density))
-	}
-        binaryMatrix[max$x == 1, max$y == 1] <- 0 # zero out this clusters region for next round
-        clusters[[i]] <- list(rows=row.names(binaryMatrix)[max$x == 1], 
-		cols=colnames(binaryMatrix)[max$y == 1], score=max$score)
+    while((length(clusters) < totalClusters) && (sum(binaryMatrix) > 0)){
+        bicluster <- .algorithmA_2(binaryMatrix, alpha, beta, minDensityP1, tries, p)
+        binaryMatrix[bicluster$rows, bicluster$cols] <- 0 # zero out this clusters region for next round
+        clusters[[i]] <- list(rows=rowCoords, cols=colCoords, score=maxCoords$score)
     }
     return(clusters)
+}
+
+# Algorithm A_2 find clusters above set density
+.algorithmA_2 <- function(binaryMatrix, alpha, beta, minDensityP1, tries, p){
+    density <- -1 
+    rowCoords <- 1:nrow(binaryMatrix)
+    colCoords <- 1:nrow(binaryMatrix)
+    while(density < minDensityP1){
+        resultCoords <- .algorithmA_1(binaryMatrix[rowCoords, colCoords], alpha, beta, tries, p)
+        rowCoords <- rowCoords[resultCoords$rows]
+        colCoords <- rowCoords[resultCoords$cols]
+        density <- sum((binaryMatrix[rowCoords,colCoords])/(length(rowCoords) * length(colCoords)))
+    }
+    return(list(rows=rowCoords, cols=colCoords))
+}
+
+# Algorithm A_1 in parallel
+.algorithmA_1 <- function(binaryMatrix, alpha, beta, tries, p){
+    allResults <- foreach(thisTry = 1:tries) %dorng% {
+        res1 <- .BicBin(binaryMatrix, alpha, beta, p, proc_genes=TRUE) # Algorithm A_1 by columns
+         res2 <- .BicBin(binaryMatrix, alpha, beta, p, proc_genes=FALSE) # Algorithm A_1 by rows
+         if(res1$score > res2$score)
+             return(res1)
+         else
+            return(res2)
+    }
+    maxCoords <- allResults[[which.max(sapply(allResults, function(x) x$score))]]
+    return(list(rows=(maxCoords$x == 1), cols=(maxCoords$y == 1), score=maxCoords$score))
 }
